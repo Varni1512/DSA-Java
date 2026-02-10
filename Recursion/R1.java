59ee9f65bdb055129f7e33ca0bc4a0db42e469ae
Varni1102 <varnikumar1512@gmail.com>
2026-02-10 23:20:07 +0530

    Recusion Basics

---
+++ Recursion/R1.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class R1 {
+
+    public static void Hello(){
+        System.out.println("Hello Varni");
+    }
+    public static void main(String[] args) {
+        Hello();
+        Hello();
+        Hello();
+    }
+}
